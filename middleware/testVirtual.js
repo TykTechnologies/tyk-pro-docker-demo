3368d53cfa53da7325fa0b3950501ddf50d1a287
Sedky <sedkyaboushamalah@gmail.com>
2020-05-29 19:44:12 -0400

    changes

--- middleware/testVirtual.js
+++
@@ -1,7 +0,0 @@
-function myUniqueFunctionName(request, session, config) {
-    var responseObject = { 
-      Body: "THIS IS A  VIRTUAL RESPONSE", 
-      Code: 200 
-    }
-    return TykJsResponse(responseObject, session.meta_data)
-  }
